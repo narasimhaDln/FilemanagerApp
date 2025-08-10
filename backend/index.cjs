f1e90b5c0f0909a168abf616ae987faedaa656cb
D.Lakshmi Narasimha <narasimha34327@gmail.com>
2025-08-10 12:28:15 +0530

    deployment error

--- backend/index.cjs
+++ backend/index.cjs
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'Frontend', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   });
 }
 app.use('/api/user', userRoutes);
